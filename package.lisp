133e0834145d86ac1c3e9fbd0bced155cabd86a9
Kilian M. Haemmerle <61506792+kilianmh@users.noreply.github.com>
2022-02-24 12:04:10 +0100

    Fix *PACKAGE* bloating in DEFPACKAGE :EXPORT clause
    Replace KEYWORD in DEFPACKAGE :EXPORT clause with UNINTERNED SYMBOL. This avoids interning them in *PACKAGE*, therefore simplifying autocompletion and allowing garbage collection.

--- package.lisp
+++ package.lisp
@@ -1,13 +1,13 @@
 (defpackage #:sparql
   (:use #:cl #:parser-combinators)
-  (:export :sparql-query
-           :sparql-query*
-           :sparql
-           :sparql-compile
-           :sparql-eval
-           :enable-uri-syntax
-           :parse-literal
-           :render-literal
-           :make-uri
-           :define-uri-prefix
-           :get-uri-prefix))
+  (:export #:sparql-query
+           #:sparql-query*
+           #:sparql
+           #:sparql-compile
+           #:sparql-eval
+           #:enable-uri-syntax
+           #:parse-literal
+           #:render-literal
+           #:make-uri
+           #:define-uri-prefix
+           #:get-uri-prefix))
